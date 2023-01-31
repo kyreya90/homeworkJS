53f6609ce6615d80f39977c1c2de8411f903cda2
kyreya90 <kyreya90@ukr.net>
2023-01-31 23:01:38 +0200

    hm1

---
+++ JSLeson1/main.js
@@ -0,0 +1,42 @@
+console.log(3347)
+let aa="hallo";
+let bb="owu";
+let cc="com";
+let d="ua";
+console.log(aa,bb,cc,d)
+let e=1;
+let f=10;
+let g=-999;
+const PI=3.14159;
+let k=2.7;
+let j=16;
+console.log(e,f,g,PI,k,j)
+console.log(j+f)
+let l=true;
+let ll=false;
+console.log(l,ll)
+let fname='Mykhaylo';
+let mname='Vasyliovych';
+let lname='Kyreya';
+let msg=fname+' '+mname+' '+lname;
+console
+console.log(msg)
+let a = 100; let b = '100'; let c = true;
+console
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+console.log (a===b)
+console.log (c===l)
+console.log (c=l)
+console.log (a<g)
+
+let person =prompt("hallo, what is your name");
+let surname=prompt("and? what is your surname");
+let old =prompt(person+' '+"how old are you");
+console.log(person)
+console.log(surname)
+console.log(old)
+
+
